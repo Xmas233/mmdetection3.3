a91150084bf064c4ba821d5f59ac45d70db499a8
Xmas233 <1379389195@qq.com>
2025-06-08 23:52:25 +0800

    updata retinanet config

--- MyModel/retinanet_r50_fpn_50e_WAID.py
+++ MyModel/retinanet_r50_fpn_50e_WAID.py
@@ -6,14 +6,16 @@ _base_ = [
 ]
 
 '''
-python tools/train.py MyModel/retinanet_r50_fpn_50e_WAID.py
+python tools/train.py MyModel/retinanet_r50_fpn_50e_WAID.py --amp
 python tools/analysis_tools/browse_dataset.py MyModel/retinanet_r50_fpn_50e_WAID.py
-python tools/analysis_tools/analyze_logs.py plot_curve work_dirs/yolox_l_8xb8-300e_WAID/20250606_182252/vis_data/20250606_182252.json --keys loss --legend Loss
-python tools/test.py MyModel/retinanet_r50_fpn_50e_WAID.py work_dirs/yolox_l_8xb8-300e_WAID/epoch_100.pth --out yolox.pkl
-python tools/analysis_tools/analyze_results.py MyModel/retinanet_r50_fpn_50e_WAID.py yolox.pkl work_dirs/yolox_l_8xb8-300e_WAID/vis --show-score-thr 0.3 
+python tools/analysis_tools/analyze_logs.py plot_curve work_dirs/retinanet_r50_fpn_50e_WAID/20250608_154657/vis_data/20250608_154657.json --keys loss --legend Loss
+python tools/test.py MyModel/retinanet_r50_fpn_50e_WAID.py work_dirs/retinanet_r50_fpn_50e_WAID/epoch_50.pth --out work_dirs/retinanet_r50_fpn_50e_WAID/retinanet_r50_fpn_50e_WAID.pkl --show-dir work_dirs/retinanet_r50_fpn_50e_WAID/vis
+python tools/analysis_tools/analyze_results.py MyModel/retinanet_r50_fpn_50e_WAID.py work_dirs/retinanet_r50_fpn_50e_WAID/retinanet_r50_fpn_50e_WAID.pkl --eval bbox --format-only --show-dir work_dirs/retinanet_r50_fpn_50e_WAID/vis work_dirs/yolox_l_8xb8-300e_WAID/vis --show-score-thr 0.3 
+python tools/analysis_tools/confusion_matrix.py MyModel/retinanet_r50_fpn_50e_WAID.py work_dirs/retinanet_r50_fpn_50e_WAID/retinanet_r50_fpn_50e_WAID.pkl work_dirs/retinanet_r50_fpn_50e_WAID --show
+python demo/large_image_demo.py data/DJI.jpg MyModel/retinanet_r50_fpn_50e_WAID.py work_dirs/retinanet_r50_fpn_50e_WAID/epoch_50.pth
 '''
 
-max_epochs = 50
+max_epochs = 100
 interval = 5
 train_cfg = dict(max_epochs=max_epochs, val_interval=interval)
 
@@ -29,11 +31,16 @@ model = dict(
         type='DetDataPreprocessor',
         # mean: [107.720 126.924 131.645]
         # std: [35.672 36.244 38.385]
-        mean=[123.675, 116.28, 103.53], #TODO: check if this is correct
-        std=[58.395, 57.12, 57.375],
+        mean=[107.720, 126.924, 131.645], #TODO: check if this is correct
+        std=[35.672, 36.244, 38.385],
         bgr_to_rgb=True,
         pad_size_divisor=32),
     
     bbox_head=dict(
         num_classes=6)
     )
+
+# hooks
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=interval, max_keep_ckpts=3),
+)
